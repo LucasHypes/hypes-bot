aabe4b10099872a6324c09495163765ab2d16495
LucasHypes <lucascferreira77@gmail.com>
2025-07-12 11:11:51 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from discord.ext import commands
 from config import DISCORD_TOKEN
 from keep_alive import keep_alive
 from monitor import monitorar_partidas
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-class HypesBot(commands.Bot):
-    async def setup_hook(self):
-        self.loop.create_task(monitorar_partidas(self, canal_nome="resultados", tag_jogador="#8VPG0PCPJ"))
-
-bot = HypesBot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'🤖 Bot conectado como {bot.user.name}')
+    
+    bot.loop.create_task(monitorar_partidas(bot, canal_nome="resultados", tag_jogador="#8VPG0PCPJ"))
 
 @bot.command()
 async def ping(ctx):
