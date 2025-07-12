65ff6fd448373562d6eb619fb0cbf7009aa89a24
LucasHypes <lucascferreira77@gmail.com>
2025-07-12 16:01:42 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
-from config import DISCORD_TOKEN
+from config import DISCORD_TOKEN, API_URL
 from keep_alive import keep_alive
 from monitor import monitorar_partidas
-import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,7 +12,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f'🤖 Bot conectado como {bot.user.name}')
-    
     bot.loop.create_task(monitorar_partidas(bot, canal_nome="resultados", tag_jogador="#8VPG0PCPJ"))
 
 @bot.command()
